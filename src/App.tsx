1437ae62cab194f24e1eacdd5719cc0fd5d1261e
jienix <524710692@qq.com>
2026-03-06 00:32:41 +0800

    Add Tailwind CSS integration and update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,21 @@
 // Redux 版本：使用 TodoList
 
 // 当前使用 MST 版本
-import BigDipper from './components/BigDipper'
+// import BigDipper from './components/BigDipper'
 // 如果要使用 Redux 版本，改为：
 // import { TodoList } from './components'
 
-import './App.css'
-import StarSky from './components/StarSky'
-import AdvancedStarrySky from './components/AdvancedStarrySky'
+// import StarSky from './components/StarSky'
+// import AdvancedStarrySky from './components/AdvancedStarrySky'
+import BigDipperApp from './components/BigDipperApp'
+
 function App() {
   return (
-    <div className="app">
+    <div className="min-h-screen p-8 md:p-4">
       {/* <BigDipper /> */}
       {/* <StarSky/> */}
-      <AdvancedStarrySky/>
+      {/* <AdvancedStarrySky/> */}
+      <BigDipperApp/>
     </div>
   )
 }
